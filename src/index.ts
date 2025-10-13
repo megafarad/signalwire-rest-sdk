939dc5bb0012cbd86adcb0c937f2f1edca174796
Chris Carrington <chris@megafarad.com>
2025-10-13 19:29:47 -0400

    (fix) Add export for PagedResponse and bump version to 0.4.1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./SignalWireRESTClient";
 export * from "./SignalWireRESTClientTypes";
+export {PagedResponse} from "./PagedResponse"
 export type * from "./SignalWireRESTClientTypes";
 export type * from "./SWMLTypes";
